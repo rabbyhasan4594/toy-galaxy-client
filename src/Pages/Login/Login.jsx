d439987d558fcb110d4ed32d2c5b8caa72a80c78
rabbyhasan <rabbyhasan4594@gmail.com>
2023-05-18 20:01:41 +0600

    login page added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { FaGoogle } from 'react-icons/fa';
 import NavigationBar from '../Shared/NavigationBar/NavigationBar';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const Login = () => {
+    const { signIn, signInWithGoogle, signInWithGithub, loading } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+
+    const [error, setError] = useState('');
+    const handleGoogleSignIn = (event) => {
+        event.preventDefault();
+        signInWithGoogle()
+            .then(result => {
+                const googleUser = result.user;
+                navigate(from, { replace: true })
+            })
+            .catch(error => {
+                console.log(error.message);
+            });
+    }
+    // if (loading) {
+    //     return  <Spinner animation="border" variant="success" />
+    // }
+    
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        form.reset();
+        signIn(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                navigate(from, { replace: true })
+            })
+            .catch(error => {
+                console.error(error.message);
+                setError(error.message);
+            })
+    }
+
     return (
         <div>
             <NavigationBar></NavigationBar>
@@ -9,32 +53,36 @@ const Login = () => {
                 <div className="hero min-h-screen bg-gray-400 ">
                     <div className="hero-content flex-col lg:flex-row lg:mt-14">
                         <div className="text-center lg:text-left lg:w-1/3 lg:me-10">
-                            
+
                             <img className='rounded-lg ' src="https://i.ibb.co/2h9ByVY/main.png" alt="" />
                         </div>
                         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-cyan-600">
-                            <form className="card-body">
+                            <form onSubmit={handleLogin} className="card-body">
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Email</span>
                                     </label>
                                     <input type="email"
-                                    name="email" placeholder="email" className="input input-bordered" />
+                                        name="email" placeholder="email" className="input input-bordered" />
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Password</span>
                                     </label>
-                                    <input type="password" 
-                                    name="password"
-                                    placeholder="password" className="input input-bordered" />
+                                    <input type="password"
+                                        name="password"
+                                        placeholder="password" className="input input-bordered" />
                                     <label className="label">
-                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                        <div> Don't Have an Account? <Link to="/registration">Register</Link></div>
                                     </label>
                                 </div>
                                 <div className="form-control mt-6">
-                                    <input className="btn bg-orange-500"type="submit" value="Login" />
+                                    <input className="btn bg-orange-500" type="submit" value="Login" />
                                 </div>
+                                <div className="form-control mt-6">
+                                    <button onClick={handleGoogleSignIn}  className="btn bg-transparent" type="submit" >  <FaGoogle  className='w=4/5 me-2'/> Login with Google </button>
+                                </div>
+                                
                             </form>
                         </div>
                     </div>
--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,7 +1,65 @@
 import React, { createContext } from 'react';
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import app from '../firebase/firebase.config';
+import { useEffect } from 'react';
+import { useState } from 'react';
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvider =new GoogleAuthProvider();
+    
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const setProfile =(name,photo,currentUser) =>{
+       return updateProfile(auth.currentUser,{
+        displayName:name , photoURL: photo
+      })
+    }
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    const signInWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth,googleProvider);
+        
+    }
+    
+    
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, loggedUser => {
+            setUser(loggedUser);
+            setLoading(false);
+        })
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+
+
+
+
+
     const authInfo = {
+        user,
+        loading,
+        signIn,
+        logOut,
+        setProfile,
+        createUser,
+        signInWithGoogle,
 
     }
     return (
