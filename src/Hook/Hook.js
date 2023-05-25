73111b702907d6c4ff82a8ebecae89f5fc0cd7fd
rabbyhasan <rabbyhasan4594@gmail.com>
2023-05-26 00:12:15 +0600

    toy galaxy client

--- src/Hook/Hook.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react"
-
-const useTitle =(hook)=>{
-    useEffect(() => {
-        document.title=`ToyGalaxy | ${hook}`;
-          
-      }, [hook]);
-}
-export default useTitle;
